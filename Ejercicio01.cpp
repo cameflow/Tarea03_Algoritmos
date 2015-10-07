0dffe61395ad11924e43ddd6768e215fd0f2bf8f
Alejandro <alex.terrazas.z@gmail.com>
2015-10-07 08:43:40 -0500

    :rocket: Add comments

--- Ejercicio01.cpp
+++ Ejercicio01.cpp
@@ -5,13 +5,13 @@
 //-------------Declaración de las funciones--------------//
 
 //Función que regresa un arreglo con las gasolineras en las que se debe de parar
-std::vector<int> paradasPorHacer(int maxKilometers, std::vector<int> gasolineras);
+std::vector<int> paradasPorHacer(int maxKm, std::vector<int> gasolineras);
 
 int main()
 {
   std::vector<int> gasolineras = {1000, 1500, 500, 700, 2000, 3000};
-  int maxKilometers = 3000;
-  std::vector<int> resultado = paradasPorHacer(maxKilometers, gasolineras);
+  int maxKm = 3000;
+  std::vector<int> resultado = paradasPorHacer(maxKm, gasolineras);
   for(int i=0; i<resultado.size(); i++)
   {
     std::cout << "El camión debe recargar gasolina en la parada " << resultado[i]+1 << std::endl;
@@ -27,14 +27,14 @@ int main()
 //En caso de que no llegue detiene en la gasolinera acutal.
 //En caso de que si llegue se revisa con la que sigue de esá y así hasta encontrar a la cual no se llegaría.
 //Se necesita saber la cantidad de km máximos que puede recorrer el camionero y se asume que siempre carga tanque lleno.
-std::vector<int>  paradasPorHacer(int maxKilometers, std::vector<int> gasolineras)
+std::vector<int>  paradasPorHacer(int maxKm, std::vector<int> gasolineras)
 {
   std::vector<int> gasolinerasAParar; //Arreglo que va a contener las gasolineras a parar
   int j = 0;
   int kmRestantes = 0; //Los km que le quedan por recorrer
   while(j != gasolineras.size()-1)
   {
-    if(kmRestantes + gasolineras[j+1] > maxKilometers)
+    if(kmRestantes + gasolineras[j+1] > maxKm)
     {
      gasolinerasAParar.push_back(j);
      kmRestantes = 0;
