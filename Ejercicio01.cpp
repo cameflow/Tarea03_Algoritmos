ac348f68cd87ab6b6f0d9ae920c2069e1d3a8c74
Alejandro <alex.terrazas.z@gmail.com>
2015-10-07 08:39:30 -0500

    :rocket: Add comments

--- Ejercicio01.cpp
+++ Ejercicio01.cpp
@@ -5,13 +5,13 @@
 //-------------Declaración de las funciones--------------//
 
 //Función que regresa un arreglo con las gasolineras en las que se debe de parar
-std::vector<int> paradasPorHacer(int maxKilometers, std::vector<int> distances);
+std::vector<int> paradasPorHacer(int maxKilometers, std::vector<int> gasolineras);
 
 int main()
 {
-  std::vector<int> distances = {1000, 1500, 500, 700, 2000, 3000};
+  std::vector<int> gasolineras = {1000, 1500, 500, 700, 2000, 3000};
   int maxKilometers = 3000;
-  std::vector<int> resultado = paradasPorHacer(maxKilometers, distances);
+  std::vector<int> resultado = paradasPorHacer(maxKilometers, gasolineras);
   for(int i=0; i<resultado.size(); i++)
   {
     std::cout << "El camión debe recargar gasolina en la parada " << resultado[i]+1 << std::endl;
@@ -21,22 +21,28 @@ int main()
 
 
 //-----------Implementación de funciones----------------//
-std::vector<int>  paradasPorHacer(int maxKilometers, std::vector<int> distances)
+
+//El algoritmo lo que hace es que revisa si con la distancia que le queda por recorrer podría saltarse
+//una gasolinera y llegar a la que sigue.
+//En caso de que no llegue detiene en la gasolinera acutal.
+//En caso de que si llegue se revisa con la que sigue de esá y así hasta encontrar a la cual no se llegaría.
+//Se necesita saber la cantidad de km máximos que puede recorrer el camionero y se asume que siempre carga tanque lleno.
+std::vector<int>  paradasPorHacer(int maxKilometers, std::vector<int> gasolineras)
 {
   std::vector<int> gasolinerasAParar; //Arreglo que va a contener las gasolineras a parar
   int j = 0;
   int kmRestantes = 0; //Los km que le quedan por recorrer
-  while(j != distances.size()-1)
+  while(j != gasolineras.size()-1)
   {
-    if(kmRestantes + distances[j+1] > maxKilometers)
+    if(kmRestantes + gasolineras[j+1] > maxKilometers)
     {
      gasolinerasAParar.push_back(j);
      kmRestantes = 0;
     }
-    else//si no es prometedor, saltárselo
+    else
     {
       j++;
-      kmRestantes+=distances[j];
+      kmRestantes+=gasolineras[j];
     }
  }
  return gasolinerasAParar;
