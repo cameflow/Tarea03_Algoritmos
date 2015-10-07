e0ee30ef0246a362afe2bd1479958de414244839
Alejandro <alex.terrazas.z@gmail.com>
2015-10-07 11:09:55 -0500

    :rocket: Add comments

--- Ejercicio03.cpp
+++ Ejercicio03.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 
+//----------------------Declaración de funciones---------------------//
 std::string obtenerSubsecuencia(const std::string primera, const std::string segunda);
 
 
@@ -14,30 +15,38 @@ int main()
 }
 
 
+//--------------------Implementación de funciones------------------//
+
+//Función que obtiene la subsecuencia
+//Recibe dos strings i empieza a comparar el caracter por carater del primero con todos los del segundo
+//En caso de que sean iguales se entra a otro ciclo donde se revisa si los siguientes si son iguales
+//Cuando dejan de ser iguales se guarda este substring y se compara si es mayor o menor al substring original
+//Si es mayor este se pone como la posible solución hasta que se encuentre otro mayor
 std::string obtenerSubsecuencia(const std::string primera, const std::string segunda)
 {
-  int tamPrimera = primera.length();
-  int tamSegunda = segunda.length();
+  int tamPrimera = primera.length(); //Longitud primer palabra
+  int tamSegunda = segunda.length(); //Longitud segunda palabra
 
-  std::string subsecuenciaMasLarga = "";
-  int tamSecuenciaMasLarga = 0;
-  std::string temp = "";
+  std::string subsecuenciaMasLarga = ""; //String resultado
+  int tamSecuenciaMasLarga = 0; //Tamaño string resultado
+  std::string temp = ""; //String temporal
 
   for(int i = 0; i<tamPrimera-1;i++)
   {
     int k = i;
     for (int j = 0; j < tamSegunda-1 ; j++)
     {
-      temp = "";
+      temp = ""; //Se vacía el string temporal
       int l = j;
-      while (primera[k] == segunda [l])
+      while (primera[k] == segunda [l]) //Se compara si las letras son iguales
       {
-        temp += primera[k];
+        temp += primera[k]; //se agrega esa letra al string emporal
         k++;
         l++;
       }
-      if(temp.length() > tamSecuenciaMasLarga)
+      if(temp.length() > tamSecuenciaMasLarga) //Si el string temporal es mayor en cuanto a tamaño este se convierte en el rsutlado
       {
+        //Se actualizan los valores del resultado y de su tamaño
         subsecuenciaMasLarga = temp;
         tamSecuenciaMasLarga = subsecuenciaMasLarga.length();
       }
